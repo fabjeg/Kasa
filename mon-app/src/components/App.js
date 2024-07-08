78295cb006d2a75906a60a4bedae963da3b65192
fabien jego <fabien.jego.p@gmail.com>
2024-07-08 18:15:24 +0200

    ajout header et banniere

--- mon-app/src/components/App.js
+++ mon-app/src/components/App.js
@@ -1,32 +1,64 @@
 import "../styles/App.scss";
 import logo from "../assets/logo.png";
+import montagne from "../assets/montagne.png";
+import { createBrowserRouter, NavLink, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Link />,
+    children: [
+      {
+        path: "contact",
+        element: <div>Accueil</div>,
+      },
+      {
+        path: "Apropos",
+        element: <div>A propos</div>,
+      },
+    ],
+  },
+]);
+
+function Link() {
+  return (
+    <>
+      <header className="link">
+        <NavLink to="/contact">Accueil</NavLink>
+        <NavLink
+          to="/Apropos"
+          className="active"
+        >
+          A propos
+        </NavLink>
+      </header>
+    </>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header>
-        <nav className="navbar">
-          <img
-            src={logo}
-            alt="logo"
-          />
-          <div className="liens">
-            <a
-              href="#"
-              className="acceuil"
-            >
-              accueil
-            </a>
-            <a
-              href="#"
-              className="apropos"
-            >
-              A propos
-            </a>
-          </div>
-        </nav>
+    <>
+      <header className="header">
+        <img
+          src={logo}
+          alt="logo"
+        />
+        <div>
+          <RouterProvider router={router} />
+        </div>
       </header>
-    </div>
+      <main>
+        <div className="container">
+          <img
+            className="mountain"
+            src={montagne}
+            alt="mountain"
+          />
+          <h1 className="test2">Chez vous, partout et ailleurs</h1>
+        </div>
+      </main>
+    </>
   );
 }
 
