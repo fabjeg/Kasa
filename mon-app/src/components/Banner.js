a52cf2361db5187374cb7d4335304c5df357cb4c
fabien jego <fabien.jego.p@gmail.com>
2024-07-23 13:55:05 +0200

    refactoring Banner

--- mon-app/src/components/Banner.js
+++ mon-app/src/components/Banner.js
@@ -1,9 +1,24 @@
+import React from "react";
 import "../styles/Banner.scss";
+
 export function Banner({ image, description }) {
   return (
     <div className="container">
-      <img className="mountain" src={image} alt="mountain" />
-      {description?.length > 0 && <h1 className="title">{description}</h1>}
+      <img
+        className="mountain"
+        src={image}
+        alt="mountain"
+      />
+      {description?.length > 0 && (
+        <h1 className="title">
+          {description.split("\n").map((line, index) => (
+            <React.Fragment key={index}>
+              {line}
+              <br />
+            </React.Fragment>
+          ))}
+        </h1>
+      )}
     </div>
   );
 }
